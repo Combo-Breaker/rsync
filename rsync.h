f4ed82faeec44df7b42eb29c860485e1476bb21b
Sarkisyan Veronica <impecopeco@gmail.com>
2015-11-11 19:35:08 +0300

    Create rsync.h

---
+++ rsync.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+enum class MsgId {
+    GETLIST = 1,
+    FILELIST = 2,
+    REMOVE = 3,
+    FILE = 4,
+    FILE_PART = 5,
+    OK = 6
+};
+
+struct Frame {
+    MsgId msg_id;
+    bool last;
+    std::string body;
+};
+
+class Connection {
+public:
+    virtual ~Connection() {}
+    virtual void WriteFrame(Frame* frame) {};
+    virtual void ReadFrame(Frame* frame) {};
+};
+
+
+class SocketConnection {
+public:
+    SocketConnection(int fd) : fd_(fd) {}
+//    ~SocketConnection();
+
+    virtual void WriteFrame(Frame* frame) {}
+    virtual void ReadFrame(Frame* frame) {}
+
+private:
+    int fd_;
+};
+
+
+struct FileList {
+    std::vector<std::string> files;
+
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int version) {
+        ar & files;
+    }
+};
+
+
+class Protocol {
+public:
+    Protocol(Connection* conn) : conn_(conn) {}
+
+    void SendGetList();
+    void SendFileList(const FileList& list);
+
+    MsgId RecvMsg(); // reads next frame
+    FileList GetFileList();
+
+private:
+    Connection* conn_;
+    Frame last_received_;
+};
+
+
+void rsync(const char* sender, const char* reciever) {};
