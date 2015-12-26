e0882e5c2e9b82302ac62480bcf9bbb7c144268a
Sarkisyan Veronica <impecopeco@gmail.com>
2015-12-26 12:14:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
+#include <string.h>
 #include "rsync.cpp"
 #include "rsync.h"
 
 int main(int argc, char const *argv[]) {
-		std::string source(argv[1]), dest(argv[2]);
-		rsync(source, dest);
+    std::string source(argv[2]), dest(argv[3]);
+	if (strcmp(argv[1], "server") == 0) {
+		rsync(source, dest, 1);			
+	} else if (strcmp(argv[1], "client") == 0) {
+		rsync(source, dest, 2);
+	}
 	return 0;
 }
